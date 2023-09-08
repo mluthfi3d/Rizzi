004e7e57d999dd9941acaafd73dfb3502614c98c
mluthfi3d <mluthfi3d@gmail.com>
2023-09-08 23:39:19 +0700

    forcing to lightmode

--- Rizzi/RizziApp.swift
+++ Rizzi/RizziApp.swift
@@ -15,6 +15,7 @@ struct RizziApp: App {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .preferredColorScheme(.light)
         }
     }
 }
